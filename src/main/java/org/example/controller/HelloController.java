8d7da4bbf047365dd83115671aec14c27bad9244
AnnLuciole <annluciole@mail.ru@>
2023-05-30 17:28:21 +0500

    Initial commit

---
+++ src/main/java/org/example/controller/HelloController.java
@@ -0,0 +1,16 @@
+package org.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello, World";
+    }
+}
